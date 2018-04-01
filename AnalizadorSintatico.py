6625dcb9e233743e7bf19645081178e34910a50c
alejosaur <asantamariap@unal.edu.co>
2018-04-01 16:18:24 -0500

    conjuntpos

--- AnalizadorSintatico.py
+++ AnalizadorSintatico.py
@@ -22,53 +22,175 @@ pila=[];
 
 ##-----------------CONJUNTOS DE PREDICCION------------------
 
-ATOM={
-'<token_integer>':['<token_integer>'],
-'<token_float>':['<token_float>'],
-'<true>':['<true>'],
-'<false>':['<false>'],
-'<token_string>':['<token_string>'],
-'<token_cor_izq>':[ARRAY],
-'<token_llave_izq':[OBJETO],
-'<id>':[VARIABLE],
-'<nil>':['<nil>']
-}
+ARRAY={}
+OBJETO={}
+VARIABLE={}
+EXPR={}
+EXPR2={}
+FACTOREXPR2={}
+AUXP={}
+AUXF={}
+PARAM={}
+FACTORAUXP={}
 
-EXPR={
-'<token_not':[EXPR2,FACTOREXPR2],
-'<token_par_izq>':[EXPR2,FACTOREXPR2],
-'<token_integer>':[EXPR2,FACTOREXPR2],
-'<token_float>':[EXPR2,FACTOREXPR2],
-'<true>':[EXPR2,FACTOREXPR2],
-'<false>':[EXPR2,FACTOREXPR2],
-'<token_string>':[EXPR2,FACTOREXPR2],
-'<nil>':[EXPR2,FACTOREXPR2],
-'<token_llave_izq>':[EXPR2,FACTOREXPR2],
-'<id>':[EXPR2,FACTOREXPR2],
-'<token_cor_izq>':[EXPR2,FACTOREXPR2]
+ATOM={
+  '<token_integer>':['<token_integer>'],
+  '<token_float>':['<token_float>'],
+  '<true>':['<true>'],
+  '<false>':['<false>'],
+  '<token_string>':['<token_string>'],
+  '<token_cor_izq>':["ARRAY"],
+  '<token_llave_izq':["OBJETO"],
+  '<id>':["VARIABLE"],
+  '<nil>':['<nil>']
 }
 
 EXPRCOMA={
-'<token_menos>':['<token_menos>',EXPR],
-'<token_not':[EXPR],
-'<token_par_izq>':[EXPR],
-'<token_integer>':[EXPR],
-'<token_float>':[EXPR],
-'<true>':[EXPR],
-'<false>':[EXPR2],
-'<token_string>':[EXPR2],
-'<nil>':[EXPR2],
-'<token_llave_izq>':[EXPR2],
-'<id>':[EXPR2],
-'<token_cor_izq>':[EXPR2]
+  '<token_menos>':['<token_menos>',"EXPR"],
+  '<token_not':["EXPR"],
+  '<token_par_izq>':["EXPR"],
+  '<token_integer>':["EXPR"],
+  '<token_float>':["EXPR"],
+  '<true>':["EXPR"],
+  '<false>':["EXPR"],
+  '<token_string>':["EXPR"],
+  '<nil>':["EXPR"],
+  '<token_llave_izq>':["EXPR"],
+  '<id>':["EXPR"],
+  '<token_cor_izq>':["EXPR"]
 }
 
-EXPR2={'<token_not>':['<token_not>',EXPR2],
-'<token_par_izq>':['<token_par_izq>',EXPRCOMA,'<token_par_der>'],
+EXPR={
+  '<token_not>':["EXPR2","FACTOREXPR2"],
+  '<token_par_izq>':["EXPR2","FACTOREXPR2"],
+  '<token_integer>':["EXPR2","FACTOREXPR2"],
+  '<token_float>':["EXPR2","FACTOREXPR2"],
+  '<true>':["EXPR2","FACTOREXPR2"],
+  '<false>':["EXPR2","FACTOREXPR2"],
+  '<token_string>':["EXPR2","FACTOREXPR2"],
+  '<nil>':["EXPR2","FACTOREXPR2"],
+  '<token_llave_izq>':["EXPR2","FACTOREXPR2"],
+  '<id>':["EXPR2","FACTOREXPR2"],
+  '<token_cor_izq>':["EXPR2","FACTOREXPR2"],
+  '<token_menos>':["EXPRCOMA"]
+}
+
+EXPR2={
+  '<token_not>':['<token_not>',"EXPR2"],
+  '<token_par_izq>':['<token_par_izq>',"EXPRCOMA",'<token_par_der>'],
+  '<token_integer>':['ATOM'],
+  '<token_float>':['ATOM'],
+  '<true>':['ATOM'],
+  '<false>':['ATOM'],
+  '<token_string>':['ATOM'],
+  '<nil>':['ATOM'],
+  '<token_llave_izq>':['ATOM'],
+  '<id>':['ATOM'],
+  '<token_cor_izq>':['ATOM']
+}
+
+
+FACTOREXPR2={
+  '<token_mul>':['<token_mul>', 'EXPR'],
+  '<token_div>':['<token_div>', 'EXPR'],
+  '<token_mod>':['<token_mod>', 'EXPR'],
+  '<token_mas>':['<token_mas>', 'EXPR'],
+  '<token_menos>':['<token_menos>', 'EXPR'],
+  '<token_mayor_igual>':['<token_mayor_igual>', 'EXPR'],
+  '<token_menor_igual>':['<token_menor_igual>', 'EXPR'],
+  '<token_mayor>':['<token_mayor>', 'EXPR'],
+  '<token_menor>':['<token_menor>', 'EXPR'],
+  '<token_igual_num>':['<token_igual_num>', 'EXPR'],
+  '<token_diff_num>':['<token_diff_num>', 'EXPR'],
+  '<token_and>':['<token_and>', 'EXPR'],
+  '<token_or>':['<token_or>', 'EXPR'],
+  '<token_pot>':['<token_pot>', 'EXPR'],
+  '<token_coma>':['epsilon'],
+  '<token_not>':['epsilon'],
+  '<token_par_izq>':['epsilon'],
+  '<token_integer>':['epsilon'],
+  '<token_float>':['epsilon'],
+  '<true>':['epsilon'],
+  '<false>':['epsilon'],
+  '<token_string>':['epsilon'],
+  '<nil>':['epsilon'],
+  '<token_cor_izq>':['epsilon'],
+  '<id>':['epsilon'],
+  '<token_llave_izq>':['epsilon'],
+  '<token_cor_der>':['epsilon'],
+  '<token_par_der>':['epsilon'],
+  '<token_point>':['epsilon'],
+  '<importar>':['epsilon'],
+  '<desde>':['epsilon'],
+  '<log>':['epsilon'],
+  '<retorno>':['epsilon'],
+  '<if>':['epsilon'],
+  '<for>':['epsilon'],
+  '<while>':['epsilon'],
+  '<funcion>':['epsilon'],
+  '<enter>':['epsilon'],
+  '<token_dosp>':['epsilon'],
+  '<token_llave_der>':['epsilon'],
+  '<end>':['epsilon']
+}
+
+FUN_STAT={
+  '<funcion>':['<funcion>','<id>','<token_par_izq>','AUXP','<token_par_der>','AUXF','<end>','<funcion>']
+}
+
+AUXP={
+  '<id>':['PARAM','FACTORAUXP'],
+  '<token_par_der>':['epsilon']
+}
+
+PARAM={
+  '<id>':['<id>','FACTORPARAM']
+}
+
+FACTORAUXP={
+  '<token_coma>':['<token_coma>','AUXP'],
+  '<token_par_der>':['epsilon']
+}
+
+FACTORPARAM={
+  '<token_igual_num>':['<token_igual_num>','EXPR'],
+  '<token_coma>':['epsilon'],
+  '<token_par_der>':['epsilon']
+}
+
+AUXF={
+  '<enter>':['<enter>','AUXF'],
+  '<token_point>':['STAT','AUXF'],
+  '<id>':['STAT','AUXF']
+  '<importar>':['STAT','AUXF'],
+  '<desde>':['STAT','AUXF'],
+  '<log>':['STAT','AUXF'],
+  '<retorno>':['STAT','AUXF']
+  '<token_integer>':['STAT','AUXF'],
+  '<token_float>':['STAT','AUXF']
+  '<true>':['STAT','AUXF'],
+  '<false>':['STAT','AUXF'],
+  '<token_string>':['STAT','AUXF'],
+  '<nil>':['STAT','AUXF']
+  '<token_llave_izq>':['STAT','AUXF'],
+  '<token_cor_izq>':['STAT','AUXF'],
+  '<if>':['STAT','AUXF'],
+  '<for>':['STAT','AUXF']
+  '<while>':['STAT','AUXF'],
+  '<funcion>':['STAT','AUXF'],
+  '<end>':['epsilon'],
+  '<token_llave_der>':['epsilon']
+}
+
+ARRAY={
   
 }
 
 SOURCE={}
+
+a=FACTOREXPR2['<token_mayor_igual>']
+
+print(a)
 
 def checkLine(T,i):
   inicio=0

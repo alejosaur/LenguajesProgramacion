70f7c86c8177615d28eee92a6945856bd62d0468
alejosaur <asantamariap@unal.edu.co>
2018-04-01 20:40:25 -0500

    mas conjuntos

--- AnalizadorSintatico.py
+++ AnalizadorSintatico.py
@@ -8,7 +8,7 @@ flotante = re.compile('^[0-9]+[.][0-9]*$')
 identificador = re.compile('^(([a-zA-Z])+[0-9]*)+$')
 tokensreservados1 = re.compile('^({$|}$|,$|:$|#$|\[$|\]$|\($|\)$|>$|<$|!$|\+$|-$|\*$|/$|%$|\^$|=$|\.$)+')
 tokensreservados2 = re.compile('^(>=$|<=$|==$|!=$|&&$|\|\|$)+')
-tokensLargos = re.compile('^(desde$|todo$|end$|retorno$|true$|false$|funcion$|retorno$|log$|end$|for$|while$|if$|in$|importar$|else$|nil$|elif$)+')
+tokensLargos = re.compile('^(desde$|todo$|end$|retorno$|true$|false$|funcion$|retorno$|log$|end$|for$|while$|if$|in$|importar$|else$|nil$|leer$|elif$)+')
 comentario = re.compile('^#.*')
 stringCompleta = re.compile('^\".*\"$')
 stringIncompleta = re.compile('^\".*')
@@ -161,21 +161,21 @@ FACTORPARAM={
 AUXF={
   '<enter>':['<enter>','AUXF'],
   '<token_point>':['STAT','AUXF'],
-  '<id>':['STAT','AUXF']
+  '<id>':['STAT','AUXF'],
   '<importar>':['STAT','AUXF'],
   '<desde>':['STAT','AUXF'],
   '<log>':['STAT','AUXF'],
-  '<retorno>':['STAT','AUXF']
+  '<retorno>':['STAT','AUXF'],
   '<token_integer>':['STAT','AUXF'],
-  '<token_float>':['STAT','AUXF']
+  '<token_float>':['STAT','AUXF'],
   '<true>':['STAT','AUXF'],
   '<false>':['STAT','AUXF'],
   '<token_string>':['STAT','AUXF'],
-  '<nil>':['STAT','AUXF']
+  '<nil>':['STAT','AUXF'],
   '<token_llave_izq>':['STAT','AUXF'],
   '<token_cor_izq>':['STAT','AUXF'],
   '<if>':['STAT','AUXF'],
-  '<for>':['STAT','AUXF']
+  '<for>':['STAT','AUXF'],
   '<while>':['STAT','AUXF'],
   '<funcion>':['STAT','AUXF'],
   '<end>':['epsilon'],
@@ -183,10 +183,214 @@ AUXF={
 }
 
 ARRAY={
-  
+  '<token_cor_izq>':['<token_cor_izq>','INTERNARRAY','<token_cor_der>']
 }
 
-SOURCE={}
+INTERNARRAY={
+  '<token_not>':['EXPR','INTERNARRAYP'],
+  '<token_par_izq>':['EXPR','INTERNARRAYP'],
+  '<token_integer>':['EXPR','INTERNARRAYP'],
+  '<token_float>':['EXPR','INTERNARRAYP'],
+  '<true>':['EXPR','INTERNARRAYP'],
+  '<false>':['EXPR','INTERNARRAYP'],
+  '<token_string>':['EXPR','INTERNARRAYP'],
+  '<nil>':['EXPR','INTERNARRAYP'],
+  '<token_cor_izq>':['EXPR','INTERNARRAYP'],
+  '<id>':['EXPR','INTERNARRAYP'],
+  '<token_llave_izq>':['EXPR','INTERNARRAYP'],
+  '<token_cor_der':['epsilon']
+}
+
+INTERNARRAYP={
+  '<token_coma>':['INTERNARRAY1'],
+  '<token_dosp>':['INTERNARRAY2']
+}
+
+INTERNARRAY1={
+  '<token_coma>':['token_coma','EXPR','INTERNARRAY1'],
+  '<token_cor_der':['epsilon']
+}
+
+INTERNARRAY2={
+  '<token_dosp>':['<token_dosp>','INTERNARRAY3']
+}
+
+INTERNARRAY3={
+  '<token_not>':['EXPR','INTERNARRAY3P'],
+  '<token_par_izq>':['EXPR','INTERNARRAY3P'],
+  '<token_integer>':['EXPR','INTERNARRAY3P'],
+  '<token_float>':['EXPR','INTERNARRAY3P'],
+  '<true>':['EXPR','INTERNARRAY3P'],
+  '<false>':['EXPR','INTERNARRAY3P'],
+  '<token_string>':['EXPR','INTERNARRAY3P'],
+  '<nil>':['EXPR','INTERNARRAY3P'],
+  '<token_cor_izq>':['EXPR','INTERNARRAY3P'],
+  '<id>':['EXPR','INTERNARRAY3P'],
+  '<token_llave_izq>':['EXPR','INTERNARRAY3P']
+}
+
+INTERNARRAY3P={
+  '<token_dosp>':['<token_dosp>','<EXPR>'],
+  '<token_cor_der>':['epsilon']
+}
+
+SOURCE={
+  '<enter>':['FROM_FILE'],
+  '<EOF>':['FROM_FILE'],
+  '<token_point>':['FROM_FILE'],
+  '<id>':['FROM_FILE'],
+  '<importar>':['FROM_FILE'],
+  '<desde>':['FROM_FILE'],
+  '<log>':['FROM_FILE'],
+  '<retorno>':['FROM_FILE'],
+  '<token_integer>':['FROM_FILE'],
+  '<token_float>':['FROM_FILE'],
+  '<true>':['FROM_FILE'],
+  '<false>':['FROM_FILE'],
+  '<token_string>':['FROM_FILE'],
+  '<nil>':['FROM_FILE'],
+  '<token_cor_izq>':['FROM_FILE'],
+  '<token_llave_izq>':['FROM_FILE'],
+  '<if>':['FROM_FILE'],
+  '<for>':['FROM_FILE'],
+  '<while>':['FROM_FILE'],
+  '<funcion>':['FROM_FILE']
+}
+
+FROM_FILE={
+  '<token_point>':['STAT','FROM_FILE'],
+  '<id>':['STAT','FROM_FILE'],
+  '<importar>':['STAT','FROM_FILE'],
+  '<desde>':['STAT','FROM_FILE'],
+  '<log>':['STAT','FROM_FILE'],
+  '<retorno>':['STAT','FROM_FILE'],
+  '<token_integer>':['STAT','FROM_FILE'],
+  '<token_float>':['STAT','FROM_FILE'],
+  '<true>':['STAT','FROM_FILE'],
+  '<false>':['STAT','FROM_FILE'],
+  '<token_string>':['STAT','FROM_FILE'],
+  '<nil>':['STAT','FROM_FILE'],
+  '<token_cor_izq>':['STAT','FROM_FILE'],
+  '<token_llave_izq>':['STAT','FROM_FILE'],
+  '<if>':['STAT','FROM_FILE'],
+  '<for>':['STAT','FROM_FILE'],
+  '<while>':['STAT','FROM_FILE'],
+  '<funcion>':['STAT','FROM_FILE'],
+  '<enter>':['enter','FROM_FILE'],
+  '<EOF>':['<EOF>']
+}
+
+STAT={
+  '<token_point>':['SIMPLE_STAT'],
+  '<id>':['SIMPLE_STAT'],
+  '<importar>':['SIMPLE_STAT'],
+  '<desde>':['SIMPLE_STAT'],
+  '<log>':['SIMPLE_STAT'],
+  '<retorno>':['SIMPLE_STAT'],
+  '<token_integer>':['SIMPLE_STAT'],
+  '<token_float>':['SIMPLE_STAT'],
+  '<true>':['SIMPLE_STAT'],
+  '<false>':['SIMPLE_STAT'],
+  '<token_string>':['SIMPLE_STAT'],
+  '<nil>':['SIMPLE_STAT'],
+  '<token_cor_izq>':['SIMPLE_STAT'],
+  '<token_llave_izq>':['SIMPLE_STAT'],
+  '<if>':['COMP_STAT'],
+  '<for>':['COMP_STAT'],
+  '<while>':['COMP_STAT'],
+  '<funcion>':['COMP_STAT'],
+}
+
+COMP_STAT={
+  '<if>':['IF_STAT'],
+  '<while>':['WHILE_STAT'],
+  '<for>':['FOR_STAT'],
+  '<funcion>':['FUN_STAT']
+}
+
+ATOM3={
+  '<token_integer>':['<token_integer>'],
+  '<token_float>':['<token_float>'],
+  '<true>':['<true>'],
+  '<false>':['<false>'],
+  '<token_string>':['<token_string>'],
+  '<token_cor_izq>':["ARRAY"],
+  '<token_llave_izq':["OBJETO"],
+  '<nil>':['<nil>']
+}
+
+SIMPLE_STAT={
+  '<id>':['ASSIGNMENT2'],
+  '<log>':['LOG'],
+  '<leer>':['LEER'],
+  '<importar>':['IMPORTAR'],
+  '<desde>':['IMPORTAR'],
+  '<retorno>':['RETORNAR'],
+  '<token_integer>':['ATOM3','<enter>'],
+  '<token_float>':['ATOM3','<enter>'],
+  '<true>':['ATOM3','<enter>'],
+  '<false>':['ATOM3','<enter>'],
+  '<token_string>':['ATOM3','<enter>'],
+  '<nil>':['ATOM3','<enter>'],
+  '<token_cor_izq>':['ATOM3','<enter>'],
+  '<token_llave_izq>':['ATOM3','<enter>'],
+}
+
+ASSIGNMENT={
+  '<id>':['VARIABLE','<token_assign>','ASSIGNMENTP']
+}
+
+ASSIGNMENTP={
+  '<token_not>':['EXPR'],
+  '<token_par_izq>':['EXPR'],
+  '<token_integer>':['EXPR'],
+  '<token_float>':['EXPR'],
+  '<true>':['EXPR'],
+  '<false>':['EXPR'],
+  '<token_string>':['EXPR'],
+  '<nil>':['EXPR'],
+  '<token_llave_izq>':['EXPR'],
+  '<id>':['EXPR'],
+  '<token_cor_izq>':['EXPR']
+}
+
+ASSIGNMENT2={
+  '<id>':['<id>','ASSIGNMENT2P']
+}
+
+ASSIGNMENT2P={
+  '<token_assign>':['<token_assign>','EXPR'],
+  '<token_par_izq>':['<token_par_izq>','AUXV','<token_par_der>'],
+  '<enter>':['epsilon'],
+  '<EOF>':['epsilon'],
+  '<id>':['epsilon'],
+  '<importar>':['epsilon'],
+  '<desde>':['epsilon'],
+  '<log>':['epsilon'],
+  '<retorno>':['epsilon'],
+  '<token_integer>':['epsilon'],
+  '<token_float>':['epsilon'],
+  '<true>':['epsilon'],
+  '<false>':['epsilon'],
+  '<token_string>':['epsilon'],
+  '<nil>':['epsilon'],
+  '<token_cor_izq>':['epsilon'],
+  '<token_llave_izq>':['epsilon'],
+  '<if>':['epsilon'],
+  '<for>':['epsilon'],
+  '<while>':['epsilon'],
+  '<funcion>':['epsilon'],
+  '<end>':['epsilon'],
+  '<token_llave_der>':['epsilon']
+}
+
+ASSIGNMENT3={
+  '<id>':['<id>','ASSIGNMENT3P']
+}
+
+ASSIGNMENT3P={
+  
+}
 
 a=FACTOREXPR2['<token_mayor_igual>']
 
